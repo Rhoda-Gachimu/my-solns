9577d8eda7e3a2e9ca1774881b09c3a80ac91f9d
rhoda <gachimurhoda@gmail.com>
2023-11-18 23:04:30 +0300

    functions

---
+++ function.js
@@ -0,0 +1,27 @@
+function num(){
+    console.log(42)
+}
+
+num()
+
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function sumLap(){
+    console.log(lap1+lap2+lap3)
+}
+sumLap()
+
+
+let lapsCompleted = 0
+
+function lapsIncrement(){
+   lapsCompleted = lapsCompleted + 1
+   console.log(lapsCompleted)
+
+}
+lapsIncrement()
+lapsIncrement()
+lapsIncrement()
