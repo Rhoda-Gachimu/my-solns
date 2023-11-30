4eec08b597f5241e4ae955017ec16e4f1838affe
rhoda gachimu <gachimurhoda@gmail.com>
2023-11-30 20:29:12 +0300

    increase and decrease challenge

---
+++ incredecre.js
@@ -0,0 +1,23 @@
+let myPoints = 3
+
+
+function add3Points(){
+   myPoints  += 3
+   
+}
+ add3Points()
+ add3Points()
+ add3Points()
+ 
+
+
+function remove1Point(){
+  myPoints -= 1
+  
+}
+
+remove1Point()
+remove1Point()
+
+console.log(myPoints)
+
