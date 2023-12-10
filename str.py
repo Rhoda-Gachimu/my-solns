5bcc8c355737136010634adc6cc640632e5c9f51
rhoda gachimu <gachimurhoda@gmail.com>
2023-12-10 19:44:30 +0300

    python strings

---
+++ str.py
@@ -0,0 +1,47 @@
+print("My favorite food ")
+
+# separate words into a new sentence
+# use backslash
+
+print("My favorite\n food ")
+
+# add a quotation mark
+print("My \"favorite\" food")
+
+greeting = "Hello you beautiful person"
+print(greeting)
+
+# concatenation
+print(greeting + " over there with a red dress on.")
+
+# functions
+
+print(greeting.lower())
+print(greeting.capitalize())
+print(greeting.upper())
+
+# true or false functions
+
+print(greeting.islower())
+print(greeting.isupper())
+print(greeting.isnumeric())
+
+# combination of two functions
+
+print(greeting.lower() .islower())
+
+# length function
+
+print (len(greeting))
+
+# index
+# a string gets indexed starting with zero
+
+print(greeting[0])
+print(greeting[1])
+# print(greeting[2])
+# print(greeting[3])
+
+# index function
+
+print(greeting.index("e"))
