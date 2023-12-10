1896b441ca91004440f15db0bc6599a4d64022b9
rhoda gachimu <gachimurhoda@gmail.com>
2023-12-10 19:46:35 +0300

    variables

---
+++ var.py
@@ -0,0 +1,9 @@
+name = "Mark"
+age = "50"
+
+
+# strings
+print("There once was a man named " + name + ",")
+print("he was "+ age + " years old.")
+print("He really liked the name " + name)
+print("but didn't like being " + age + ".")
