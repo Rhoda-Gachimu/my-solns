9cb3f55c360c0b71ab7400800ac4df01a3a020a2
rhoda gachimu <gachimurhoda@gmail.com>
2023-12-11 21:09:43 +0300

    numbers

---
+++ num.py
@@ -0,0 +1,38 @@
+from math import*   
+# math module
+
+print (2.55)
+print (3*(4+5))
+
+# modulus // spits out the remainder
+
+print(10%3)
+
+my_num = 55
+print(my_num)
+
+# when you want to print out numbers alongside strings
+print(str (my_num) + " is my favorite number")
+
+# functions
+    # Absolute value
+my_number = -663
+print(abs(my_number))
+
+# power
+
+print(pow(6, 3))
+
+# max  //  returns the larger number
+print(max(1,2,6))
+
+# min
+print(min(1,2,6))
+
+# round
+print(round(3.45))
+print(round(3.75))
+
+print(floor(3.7))
+print(ceil(3.45))
+print(sqrt(196))
