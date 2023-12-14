985bc05d70890fa6f521b0d51800c11e842d205f
rhoda gachimu <gachimurhoda@gmail.com>
2023-12-14 21:04:51 +0300

    lists

---
+++ lists.py
@@ -0,0 +1,16 @@
+# put related values together
+
+friends =["John" , "Mark", "Judas", "Peter"]
+
+print(friends[1])
+
+print(friends[2:])
+
+print(friends[1:])
+
+# will grab items [1] upto [3] but not item at index 3
+print(friends[1:3])
+
+# modify values inside arrays
+friends[2] = "Rhoda"
+print(friends[2])
