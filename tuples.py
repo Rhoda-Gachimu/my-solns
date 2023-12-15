eb7966ebe9422e6209775df69e2502bf4759ed39
rhoda gachimu <gachimurhoda@gmail.com>
2023-12-15 19:13:33 +0300

    tuples

---
+++ tuples.py
@@ -0,0 +1,7 @@
+# A tuple is a type of data structure where different values can be stored
+# examples of a tuple is coordinates
+# What makes tuples unique is that they are immutable(can't change anything)
+# tuples are indexed starting at zero
+
+coordinates = (2,4)
+print(coordinates[1])
