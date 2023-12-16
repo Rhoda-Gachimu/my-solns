b440330cf81b29f94a8118e2ab16f0c588720d46
rhoda gachimu <gachimurhoda@gmail.com>
2023-12-16 21:34:55 +0300

    parameters

---
+++ function.py
@@ -0,0 +1,17 @@
+# use an underscore(more than 2 words) or none for the function name
+# all lower case
+# A parameter is a piece of information given to a function
+
+def sayhi():
+    print("Hello, Rhoda!")
+
+sayhi()
+
+# parameters
+
+def sayhello(name, age):
+    print("Hello " + name + ", you are " + age + " years old!")
+
+sayhello("Adrian", "56")
+sayhello("Ethan", "36")
+sayhello("Rhoda", "16")
