beffc264bae205a503c09b4eeac9e50893466ec1
rhoda gachimu <gachimurhoda@gmail.com>
2023-12-19 23:44:45 +0300

    or & and

--- if statements.py
+++ if statements.py
@@ -1,9 +1,18 @@
 
 is_male = True
+is_tall= True
 
+# keywords
+# OR
+# AND
 
-if is_male:
-    print("You are a male")
+if is_male or is_tall:
+    print("You are a male or tall or both")
 else:
-    print("You are not a male")
+    print("You are neither male nor tall")
  
+
+if is_male and is_tall:
+    print("You are a tall male")
+else:
+    print("You are either not male or not tall or both")
