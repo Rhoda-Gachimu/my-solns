b1d93f04951ee0070df0559353c291ae635e4649
rhoda gachimu <gachimurhoda@gmail.com>
2023-12-19 23:52:06 +0300

    elseif

--- if statements.py
+++ if statements.py
@@ -1,6 +1,6 @@
 
 is_male = True
-is_tall= True
+is_tall= False
 
 # keywords
 # OR
@@ -16,3 +16,18 @@ if is_male and is_tall:
     print("You are a tall male")
 else:
     print("You are either not male or not tall or both")
+
+
+# elif (elseif)
+if is_male and is_tall:
+    print("You are a tall male")
+
+elif is_male and not(is_tall):
+    print("You are a short male")
+
+elif not(is_male) and is_tall:
+    print("You are not a male but are tall")
+
+
+else:
+    print("You are not a male or not tall")
